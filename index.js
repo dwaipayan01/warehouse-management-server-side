a80324c7a64dd8d5497526b5b41af9299a411fe8
dwaipayan01 <dwaipayanjoy1998@gmail.com>
2022-06-30 23:20:04 +0600

    my second commit

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yhiei.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("productCollection").collection("product");
+        app.use("/product", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
     res.send("My assignment is ready");
 });
