d4a91522058200852f28a1ac7d464c8bd88a3252
dwaipayan01 <dwaipayanjoy1998@gmail.com>
2022-07-01 11:35:40 +0600

    my third commit

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yhiei.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db("productCollection").collection("product");
-        app.use("/product", async (req, res) => {
+        app.get("/product", async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        });
+        app.delete("product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
         })
+
     }
     finally {
 
