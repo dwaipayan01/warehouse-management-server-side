7f9dc9e4750cb5491058b9ecd754036beb6695be
dwaipayan01 <dwaipayanjoy1998@gmail.com>
2022-07-10 20:49:46 +0600

    my sixth commit

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await productCollection.findOne(query);
+            res.send(service);
+        })
 
     }
     finally {
