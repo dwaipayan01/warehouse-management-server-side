ddb3e3a68d9958b7cf2b45a9b3f480d44383fc26
dwaipayan01 <dwaipayanjoy1998@gmail.com>
2022-07-14 11:25:45 +0600

    my eight commit

--- index.js
+++ index.js
@@ -71,6 +71,20 @@ async function run() {
             const result = await productCollection.updateOne(filter, updatedDoc, options);
             res.send(result);
         });
+        app.get("/itemList", async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            const query = { email: email };
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.delete("/item/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
 
 
     }
